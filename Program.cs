b2fad2b64c4177aece86f88478e95251741cd23f
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-11-02 22:08:58 +0100

    Gjort koden lidt mere brugervenlig

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         while (true)
         {
-            Console.WriteLine("Enter 'k' to generate a random number between 0-100 or 'q', 'quit', or 'exit' to exit");
+            Console.WriteLine("Enter 'g' or 'generate' to generate a random number between 0-100 or 'q', 'quit', or 'exit' to exit");
             string input = Console.ReadLine().ToLower();
 
             if (input == "q" || input == "quit" || input == "exit")
@@ -14,15 +14,19 @@ class Program
                 break;
             }
 
-            if (input == "k")
+            if (input == "g" || input == "generate")
             {
                 Random random = new Random();
                 int random_number = random.Next(101);
                 Console.WriteLine("Random number = " + random_number);
             }
+            else if (input == "c" || input == "continue")
+            {
+                continue;
+            }
             else
             {
-                Console.WriteLine("Invalid input. Enter 'k' or 'q', 'quit', or 'exit'.");
+                Console.WriteLine("Invalid input. Enter 'q', 'quit', or 'exit' to exit.");
             }
         }
     }
