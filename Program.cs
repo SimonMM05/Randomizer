43b42d31332b6731a863e03f0c1e422e925b2e12
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-11-02 19:17:04 +0100

    Randomizer code

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int random_number = random.Next(101);
+        Console.WriteLine("Random number = " + random_number);
+    }
+}
