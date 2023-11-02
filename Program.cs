63be9be70b9ae5973d44760668c4e6655df86447
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-11-02 20:10:36 +0100

    Færdig med kode

--- Program.cs
+++ Program.cs
@@ -4,8 +4,26 @@ class Program
 {
     static void Main()
     {
-        Random random = new Random();
-        int random_number = random.Next(101);
-        Console.WriteLine("Random number = " + random_number);
+        while (true)
+        {
+            Console.WriteLine("Enter 'k' to generate a random number between 0-100 or 'q', 'quit', or 'exit' to exit");
+            string input = Console.ReadLine().ToLower();
+
+            if (input == "q" || input == "quit" || input == "exit")
+            {
+                break;
+            }
+
+            if (input == "k")
+            {
+                Random random = new Random();
+                int random_number = random.Next(101);
+                Console.WriteLine("Random number = " + random_number);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Enter 'k' or 'q', 'quit', or 'exit'.");
+            }
+        }
     }
 }
