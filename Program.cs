f93da26d25361730ea0b66be9b7f04b8a26ae1b7
Simon Myrvig <simo008s@mail.hansenberg.dk>
2023-11-03 11:53:06 +0100

    Mere komplicerede løsning

--- Program.cs
+++ Program.cs
@@ -4,29 +4,34 @@ class Program
 {
     static void Main()
     {
+        Console.WriteLine("Hello! Welcome to my program. This program allows you to generate a random number between 0-100.");
+        Console.WriteLine("_______________________________________________________________________________________________");
+        
         while (true)
         {
-            Console.WriteLine("Enter 'g' or 'generate' to generate a random number between 0-100 or 'q', 'quit', or 'exit' to exit");
+            Console.WriteLine("Enter 'g' or 'generate' to generate a random number between 0-100, or 'q', 'quit', or 'exit' to exit.");
             string input = Console.ReadLine().ToLower();
 
             if (input == "q" || input == "quit" || input == "exit")
             {
                 break;
             }
-
-            if (input == "g" || input == "generate")
+            else if (input == "g" || input == "generate")
             {
                 Random random = new Random();
                 int random_number = random.Next(101);
                 Console.WriteLine("Random number = " + random_number);
-            }
-            else if (input == "c" || input == "continue")
-            {
-                continue;
+                Console.WriteLine("Press 'c' key to continue or any other key to exit.");
+                string continueInput = Console.ReadLine().ToLower();
+
+                if (continueInput != "c")
+                {
+                    break;
+                }
             }
             else
             {
-                Console.WriteLine("Invalid input. Enter 'q', 'quit', or 'exit' to exit.");
+                Console.WriteLine("Invalid input. Enter 'g' or 'generate' to generate a number, or 'q', 'quit', or 'exit' to exit.");
             }
         }
     }
